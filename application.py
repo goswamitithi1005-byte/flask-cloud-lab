fa99ed98303c9e63fbd6f8bc58c1cc62f7b6968f
Tithi Goswami <goswamitithi1005@gmail.com>
2026-02-16 18:58:01 +0530

    add home route

--- application.py
+++ application.py
@@ -1,14 +1,22 @@
-from flask import Flask,jsonify,request
+from flask import Flask, jsonify, request
 import os
+
 app = Flask(__name__)
-@app.route('/api/greet',methods=['GET'])
+
+@app.route("/")
+def home():
+    return "Flask Cloud Web Service is running!"
+
+@app.route('/api/greet', methods=['GET'])
 def greet():
-    return jsonify({'message':'Hello from the Cloud Web Service!'})
-@app.route('/api/sum',methods=['POST'])
+    return jsonify({'message': 'Hello from the Cloud Web Service!'})
+
+@app.route('/api/sum', methods=['POST'])
 def sum_values():
     data = request.json
-    result = data['a']+data['b']
-    return jsonify({'sum':result})
-if __name__=='__main__':
-    port = int(os.environ.get("PORT", 5000))  # Dynamic port for cloud
+    result = data['a'] + data['b']
+    return jsonify({'sum': result})
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
